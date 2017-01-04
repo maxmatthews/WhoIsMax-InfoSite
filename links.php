7d7f13c5f890769266cdb1b221dad563aa7992cb
Max Matthews <max@mzmtech.com>
2017-01-04 14:16:05 -0500

    Add quote page

--- links.php
+++ links.php
@@ -1,12 +1,13 @@
 <div class="links text-center">
-        <a href="/">Home</a> |
-        <a target="_blank" href="/cal">Calendar</a> |
-        <a href="/credits.php">Credits</a> |
-        <a target="_blank" href="//linkedin.com/in/whoisMax"><i class="fa fa-linkedin-square" aria-hidden="true"></i></i></a> |
-        <a target="_blank" href="//twitter.com/teckno101"><i class="fa fa-twitter-square" aria-hidden="true"></i></a> |
-        <a target="_blank" href="//medium.com/@maxmatthews"><i class="fa fa-medium" aria-hidden="true"></i></a> |
-        <a target="_blank" href="/MatthewsMaxResume.pdf">resume PDF</a> |
-        <a href="/map.php">travel map</a> |
-        <a href="/archives">archives</a> |
-        <a href="mailto:website@maxmatthe.ws">Contact</a>
+    <a href="/">Home</a> |
+    <a target="_blank" href="/cal">Calendar</a> |
+    <a href="/quote.php">Quote</a> |
+    <a href="/credits.php">Credits</a> |
+    <a target="_blank" href="//linkedin.com/in/whoisMax"><i class="fa fa-linkedin-square" aria-hidden="true"></i></i></a> |
+    <a target="_blank" href="//twitter.com/teckno101"><i class="fa fa-twitter-square" aria-hidden="true"></i></a> |
+    <a target="_blank" href="//medium.com/@maxmatthews"><i class="fa fa-medium" aria-hidden="true"></i></a> |
+    <a target="_blank" href="/MatthewsMaxResume.pdf">resume PDF</a> |
+    <a href="/map.php">travel map</a> |
+    <a href="/archives">archives</a> |
+    <a href="mailto:website@maxmatthe.ws">Contact</a>
 </div>
---
+++ quote.php
@@ -0,0 +1,28 @@
+<!--Are you snooping in my source code? Don't bother, it's a public git repo here:
+https://github.com/maxmatthews/WhoIsMax-InfoSite -->
+<html>
+
+<head>
+    <?php include 'head.php'; ?>
+</head>
+
+<body id="nowBody">
+
+<div class="container-fluid" id="now">
+    <div class="row" id="content">
+        <div class="text-center">
+            <h3>Quote of Month:</h3>
+            <h1>"Mind over mattress"</h1>
+            <h2>-Stephen Covey</h2>
+        </div>
+    </div>
+    <?php include 'links.php'; ?>
+</div>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+<script src="//cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
+<script src="typed.js"></script>
+</body>
+
+</html>
