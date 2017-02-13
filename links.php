5393211445e86485e4bd4b4f0623f3f9c68e6db4
Max Matthews <max@mzmtech.com>
2017-02-13 10:45:19 -0500

    Update medium link to my new publication instead of my personal profile. Will be moving to a custom subdomain soon for SEO

--- links.php
+++ links.php
@@ -5,7 +5,7 @@
     <a href="/credits.php">Credits</a> |
     <a target="_blank" href="//linkedin.com/in/whoisMax"><i class="fa fa-linkedin-square" aria-hidden="true"></i></i></a> |
     <a target="_blank" href="//twitter.com/teckno101"><i class="fa fa-twitter-square" aria-hidden="true"></i></a> |
-    <a target="_blank" href="//medium.com/@maxmatthews"><i class="fa fa-medium" aria-hidden="true"></i></a> |
+    <a target="_blank" href="//medium.com/mzm"><i class="fa fa-medium" aria-hidden="true"></i></a> |
     <a target="_blank" href="/MatthewsMaxResume.pdf">resume PDF</a> |
     <a href="/map.php">travel map</a> |
     <a href="/archives">archives</a> |
